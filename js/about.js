779072bcb733d4f6fa11aa9947b94b3ac53de108
Michael Shen <mjlshen@yahoo.com>
2014-09-10 01:17:49 -0400

    Derek's Suggestions
    Locked About Me Header, added it's own about.js, about.css

---
+++ js/about.js
@@ -0,0 +1,24 @@
+/**
+ * cbpAnimatedHeader.js v1.0.0
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2013, Codrops
+ * http://www.codrops.com
+ */
+var cbpAnimatedHeader = (function() {
+
+	var docElem = document.documentElement,
+		header = document.querySelector( '.cbp-af-header' ),
+		didScroll = false,
+		changeHeaderOn = 300;
+
+	function init() {
+		window.onload(classie.add( header, 'cbp-af-header-shrink' ));
+	}
+
+	init();
+
+})();
